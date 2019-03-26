984ce0fce09756844c824f08ee368e8cf6ed30b5
CanB <can.bas@adesso.com.tr>
2019-03-26 23:46:29 +0300

    *Whole Project again

--- FirsWeek/ViewController.swift
+++ FirsWeek/ViewController.swift
@@ -8,18 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    var viewCells = [String]()
+    
+    @IBAction func deleteButton(_ sender: UIButton) {
+        viewCells.removeAll()
+        tableView.reloadData()
+        let userDefault = UserDefaults.standard
+        userDefault.removeObject(forKey: "cell")
+        userDefault.synchronize()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        //tableView.reloadData()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+       //print(vi)
+        if let string2 = UserDefaults.standard.array(forKey: "cell") {
+            
+            viewCells = string2 as! [String]
+                        
+        }
+        
+        tableView.reloadData()
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            
+            let cell = UITableViewCell()
+            let cellString = viewCells[indexPath.row]
+            cell.textLabel?.text = cellString
+            return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewCells.count
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            viewCells.remove(at: indexPath.row)
+            tableView.reloadData()
+            let userDefault = UserDefaults.standard
+            var myarray = userDefault.array(forKey: "cell")
+            myarray?.remove(at: indexPath.row)            
+            userDefault.set(myarray, forKey: "cell")
+        }
+    }
+    
 }
 
--- FirsWeek/ViewTwo.swift
+++ FirsWeek/ViewTwo.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+import UIKit
+
+class ViewTwo: UIViewController {
+
+    var cellText = "";
+    @IBAction func saveButton(_ sender: Any) {
+        self.cellText = textField.text!
+        performSegue(withIdentifier: "segueName", sender: self)
+    }
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if let string2 = UserDefaults.standard.array(forKey: "cell") {
+            var array = string2
+            
+            if !self.cellText.isEmpty {
+            array.insert(self.cellText, at: 0)
+            let userDefaults = UserDefaults.standard
+            userDefaults.set(array, forKey: "cell")
+            }
+        } else {
+            var array: [String] = [String]()
+            array.append(self.cellText)
+            let userDefaults = UserDefaults.standard
+            userDefaults.set(array, forKey: "cell")
+        }
+    }
+
+}
